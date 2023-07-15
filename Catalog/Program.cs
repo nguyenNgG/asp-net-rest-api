b9d01215b0bc53e27e145f85aeaeb41fd6545dbe
ngianguyen81 <ngianguyen81@gmail.com>
2023-07-15 17:21:07 +0700

    add: database user-secrets, health checks

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,6 +1,9 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Repositories;
 using Catalog.Settings;
 using Catalog.Utilities;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
@@ -17,10 +20,13 @@ var builder = WebApplication.CreateBuilder(args);
 BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
+var mongoDbSettings = builder.Configuration
+    .GetSection(nameof(MongoDbSettings))
+    .Get<MongoDbSettings>();
+
 builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
 {
-    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    return new MongoClient(settings?.ConnectionString);
+    return new MongoClient(mongoDbSettings?.ConnectionString);
 });
 
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
@@ -34,6 +40,16 @@ builder.Services.AddControllers(o =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure health checks
+builder.Services
+    .AddHealthChecks()
+    .AddMongoDb(
+        mongoDbSettings!.ConnectionString!,
+        name: "mongodb",
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] { "ready" }
+    );
 
 var app = builder.Build();
 
@@ -49,5 +65,41 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Configure health check endpoints
+//// Service is ready to go i.e. database can serve requests
+app.MapHealthChecks(
+    "/health/ready",
+    new HealthCheckOptions
+    {
+        Predicate = (check) => check.Tags.Contains("ready"),
+        ResponseWriter = async (context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new
+                {
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select(
+                        entry =>
+                            new
+                            {
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception != null
+                                    ? entry.Value.Exception.Message
+                                    : "none",
+                                duration = entry.Value.Duration.ToString(),
+                            }
+                    )
+                }
+            );
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    }
+);
+
+//// Service is up and running
+app.MapHealthChecks("/health/live", new HealthCheckOptions { Predicate = (_) => false });
 
 app.Run();
